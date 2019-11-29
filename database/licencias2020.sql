847b84f620e79c52af61429dd9d60c60c1914eae
Santiago Chialvo <santiago.c@scopicsoftware.com>
2019-11-28 21:49:55 -0300

    Archive with 2020 licenses

---
+++ database/licencias2020.sql
@@ -0,0 +1,5 @@
+insert into Solicitud values(default,1,7,'20/01/2020','02/02/2020','Dias Licencia','A',null,0,1); -- Fernando
+insert into Solicitud values(default,3,7,'06/01/2020','19/01/2020','Dias Licencia','A',null,0,1); -- Mauricio
+insert into Solicitud values(default,4,7,'23/03/2020','05/04/2020','Dias Licencia','A',null,0,1); -- Angelica
+insert into Solicitud values(default,5,7,'03/02/2020','16/02/2020','Dias Licencia','A',null,0,1); -- Horacio
+insert into Solicitud values(default,6,7,'26/02/2020',null,'Dias Licencia','A',0,0,1); -- Camila
